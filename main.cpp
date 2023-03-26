8960d09140511d66f6e7731a7ea9fd4c5c6d292b
Miron (Romanov) <miron.romanov@yandex.ru>
2023-03-26 15:45:47 +0300

    code: изменение кода

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -7,5 +7,9 @@ int main()
     cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
-    cout << "A + B = " << a + b << '\n' << "A - B = " << a - b << '\n' << "A * B = " << a * b << '\n' << "A / B = " << a / b << '\n';
+    cout << "A + B = " << a + b << '\n' 
+	 << "A - B = " << a - b << '\n' 
+	 << "A * B = " << a * b << '\n' 
+	 << "A / B = " << a / b << '\n'
+	 << "MAX = " << max(a, b) << '\n';
 }
